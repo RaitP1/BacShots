29652f43c3cc580df9aa5cc6d5a5bc2133392dc7
RaitP1 <150583432+RaitP1@users.noreply.github.com>
2024-10-11 21:33:30 +0300

    EnemyMob script

---
+++ gamecamp/enemy_mob.gd
@@ -0,0 +1,29 @@
+extends Sprite2D
+
+var speed = 75
+var velocity = Vector2()
+var hp = 3
+var blood_particles = preload("res://blood_particles.tscn")
+
+func _process(delta):
+	look_at(Global.player.global_position)
+	
+	if Global.player != null:
+		if Global.gameOff == False:
+			velocity = global_position.direction_to(Global.player.global_position)
+		else:
+			velocity = Vector2(0, 0)
+			
+	if hp <= 0:
+		if Global.Camera != null:
+			Global.Camera.screen.shake(30, 0.1)
+			
+			Global.points += 10  # 10 points for killing enemy
+			if Global.node_creation.parent != null:
+				var blood_paricles_instance = Global.instance_node(blood_particles, global_position, Global.node_creation_parent)
+				blood_particles.instance.rotation = velocity.angle()
+			queue_free()
+
+
+func _on_hitbox_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
