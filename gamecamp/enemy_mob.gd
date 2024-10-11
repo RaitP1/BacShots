ba8db7fdbd7269065087953dbe3cafc3ee1cf474
RaitP1 <150583432+RaitP1@users.noreply.github.com>
2024-10-12 01:51:08 +0300

    AGAIN ALOTTTT

--- gamecamp/enemy_mob.gd
+++ gamecamp/enemy_mob.gd
@@ -3,7 +3,6 @@ extends Sprite2D
 var speed = 75
 var velocity = Vector2()
 var hp = 3
-var blood_particles = preload("res://blood_particles.tscn")
 
 func _process(_delta):
 	look_at(Global.player.global_position)
@@ -19,17 +18,17 @@ func _process(_delta):
 	if hp <= 0:
 		if Global.Camera != null:
 			Global.Camera.screen_shake(30, 0.1)
-			
 			Global.points += 10  # 10 points for killing enemy
-			if Global.node_creation_parent != null:
-				var blood_paricles_instance = Global.instance_node(blood_particles, global_position, Global.node_creation_parent)
-				blood_paricles_instance.rotation = velocity.angle()
 			queue_free()
 
 
-func _on_hitbox_area_entered(area: Area2D) -> void:
+func _on_hitbox_area_entered(area):
 	if area.is_in_group("enemy_damager"):
 		modulate = Color("ff0056")
 		velocity = -velocity * 6
 		hp -= 1
+		$hit_indicator.start()
 		area.get_parent().queue_free()
+
+func _on_hit_indicator_timeout():
+	modulate = Color("ffffff")
