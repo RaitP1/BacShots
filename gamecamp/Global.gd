7484fc79be2d00984cb59e9cba03ef7fb1fc5336
RaitP1 <150583432+RaitP1@users.noreply.github.com>
2024-10-13 10:56:45 +0300

    Initial Commit

--- gamecamp/Global.gd
+++ gamecamp/Global.gd
@@ -7,7 +7,8 @@ var points = 0
 var Camera = null
 var highscore = 0
 var gameOff = false
-var playerSpeed: int = 500
+var playerSpeed: int = 600
+var playerRun: int = 400
 var enemySpeed: int = 100
 var canShoot: bool = true
 var reloadSpeed: float = 0.5
@@ -15,7 +16,13 @@ var boss_alive1 = false
 var boss_alive2 = false
 var boss_alive3 = false
 var boss_alive4 = false
+var boss_hp = 10
+var boss_speed: int = 150
 var rounds = 1
+var boss_num = 2
+var boss_name = ""
+var inversion_x = false
+var inversion_y = false
 
 func instance_node(node, location, parent):
 	var node_instance = node.instantiate()
--- gamecamp/arena.gd
+++ gamecamp/arena.gd
@@ -4,6 +4,9 @@ var zombie_count = 0
 var round = 1
 var boss_spawned = false
 var soap_boss_packed = preload("res://boss_mobs/Soap/Boss_Soap.tscn")
+var blob_boss_packed = preload("res://boss_mobs/Blob/boss_blob.tscn")
+var sponge_boss_packed = preload("res://boss_mobs/Sponge/boss_sponge.tscn")
+var sprei_boss_packed = preload("res://boss_mobs/Sprei/boss_sprei.tscn")
 
 func _ready():
 	# Set up the timer
@@ -19,24 +22,50 @@ func spawn_zombie():
 	add_child(new_mob)
 	zombie_count += 1
 
-func spawn_boss():
+func spawn_blob_boss():
+	var blob_boss = blob_boss_packed.instantiate()
+	%PathFollow2D.progress_ratio = randf()
+	blob_boss.global_position = %PathFollow2D.global_position
+	add_child(blob_boss)
+	Global.boss_alive4 = true
+
+func spawn_soap_boss():
 	var soap_boss = soap_boss_packed.instantiate()
 	%PathFollow2D.progress_ratio = randf()
 	soap_boss.global_position = %PathFollow2D.global_position
 	add_child(soap_boss)
 	Global.boss_alive4 = true
 
+func spawn_sprei_boss():
+	var sprei_boss = sprei_boss_packed.instantiate()
+	%PathFollow2D.progress_ratio = randf()
+	sprei_boss.global_position = %PathFollow2D.global_position
+	add_child(sprei_boss)
+	Global.boss_alive4 = true
+
+func spawn_sponge_boss():
+	var sponge_boss = sponge_boss_packed.instantiate()
+	%PathFollow2D.progress_ratio = randf()
+	sponge_boss.global_position = %PathFollow2D.global_position
+	add_child(sponge_boss)
+	Global.boss_alive4 = true
+
 func _on_wave_timer_timeout():
 	print("Wave ", round, " starting!")
-	
+	print(Global.boss_name)
 	if round % 5 == 0 and not boss_spawned:
-		spawn_boss()
+		if Global.boss_num == 1:
+			spawn_blob_boss()
+			Global.boss_name = "Blob"
+		if Global.boss_num == 2:
+			spawn_soap_boss()
+			Global.boss_name = "Soap"
 		boss_spawned = true
 	else:
 		if Global.boss_alive4 == false:
 			call_zombie_wave(round)
 	
-	round += 1
+	round += 9
 	
 	# Adjust the timer for the next wave
 	var next_wave_time = 10.0 + log(round)  # Adjust timing as needed
